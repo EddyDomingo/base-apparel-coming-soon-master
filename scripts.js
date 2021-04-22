ae4bc13cc4b7fe4c2dfc92d6fd9c08e7fe0b768b
EddyDomingo <68975969+EddyDomingo@users.noreply.github.com>
2021-04-22 12:10:34 -0600

    desktop-img edit

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 console.log("hello Eddy")
+
+document.getElementById('arrowButton').addEventListener("click", validateForm);
+
+
+function validateForm(){
+    let input = document.getElementById('inputField').value;
+    const mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (input == ""){
+        alert("Oops! Please add your email");
+    }else{
+        if ( input.match(mailFormat) )
+        {
+          alert("Request submitted!")
+          return (true)
+        }
+          alert("Oops! Please check your email")
+          return (false)
+      }
+    
+    input = '';
+    }
